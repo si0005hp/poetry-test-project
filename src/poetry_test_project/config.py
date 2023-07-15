98a5b90a2d10c8193fd90b6d636900d529f3bf4d
si0005hp <si0005hp@gmail.com>
2023-07-15 19:25:22 +0900

    fix

--- src/poetry_test_project/config.py
+++ src/poetry_test_project/config.py
@@ -1,13 +1,18 @@
+import os
 import pkgutil
 from typing import Any
 
+from utils import read_parquet_as_list
 import yaml
 
 
 class Config:
     def __init__(self, env: str):
-        data = yaml.safe_load(pkgutil.get_data(__name__, "config.yml"))
-        self.config: dict = data[env]
+        config = yaml.safe_load(pkgutil.get_data(__name__, "config.yml"))
+        self.config: dict = config[env]
+
+        data_path = os.path.join(os.path.dirname(__file__), "data.parquet")
+        self.data: list[dict] = read_parquet_as_list(data_path)
 
     def __getattr__(self, key) -> Any:
         try:
@@ -19,3 +24,4 @@ class Config:
 if __name__ == "__main__":
     config = Config("development")
     print(config.model_version)
+    print(config.data)
